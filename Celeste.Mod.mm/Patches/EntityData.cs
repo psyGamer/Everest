df5e151739d1f4fcfcc573300284a151b862373a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2024-12-19 23:57:16 -0700

    Changed pattern matching back to ToString() call

--- Celeste.Mod.mm/Patches/EntityData.cs
+++ Celeste.Mod.mm/Patches/EntityData.cs
@@ -21,8 +21,8 @@ namespace Celeste {
         /// <returns></returns>
         public string String(string key, string defaultValue = null) {
             if (Values != null && Values.TryGetValue(key, out object value)
-                && value is string val && val.Trim().Length > 0) {
-                    return val;
+                && value != null && value.ToString().Trim().Length > 0) {
+                    return value.ToString();
             }
             return defaultValue;
         }
